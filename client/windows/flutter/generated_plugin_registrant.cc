bdb7d1703988ea9ecf432ad0868d5c32add7625e
Mike Nimer <mnimer@gmail.com>
2023-12-25 00:03:57 -0600

    Add windows, and linux platforms and redo mac to fix build

---
+++ client/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
+#include <realm/realm_plugin.h>
+#include <url_launcher_windows/url_launcher_windows.h>
+#include <window_size/window_size_plugin.h>
+#include <window_to_front/window_to_front_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
+  RealmPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("RealmPlugin"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
+  WindowSizePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowSizePlugin"));
+  WindowToFrontPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowToFrontPlugin"));
+}
