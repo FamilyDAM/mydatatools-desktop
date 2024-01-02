59372a1df75111a1a38438a77a8862b34be16f0b
Mike Nimer <mnimer@gmail.com>
2024-01-02 12:45:58 -0600

    remove pods folder

--- client/macos/Pods/Target Support Files/realm/realm-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_realm : NSObject
-@end
-@implementation PodsDummy_realm
-@end
--- client/macos/Pods/Target Support Files/window_size/window_size-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_window_size : NSObject
-@end
-@implementation PodsDummy_window_size
-@end
